0532a6e088e81f8d5230582424c5d30ead9a052a
Ketan Saxena <ksaxena@grepruby.com>
2018-07-20 21:08:18 +0530

    Adds auto detect feature for json/yaml schema

--- help.js
+++ help.js
@@ -7,8 +7,8 @@ const help = () => {
   Description:
     Validate is a utility used to check the structure of
     a yaml/json file against a predefined schema. The schema is expected
-    to be a js file with schema as described in the link below:
-    https://www.npmjs.com/package/schema-validator
+    to be a json or yaml/yml file having structure as defined here:
+    https://github.com/ketanTechracers/schema-validator
 
   Usage: npm run validate -- [options]
 
@@ -16,8 +16,7 @@ const help = () => {
 
   Options:
     -f, --filePath <filePath> : path to the target file for validating
-    -j, --json                : passed if target file is in JSON format
-    -s, --schema [schemaPath] : path to an external schema.js file
+    -s, --schema [schemaPath] : path to an external schema file in yml/json format
   `)
 }
 
