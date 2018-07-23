e99f9ed0a5147ce4805c06109e77ff43903053d8
Ketan Saxena <ksaxena@grepruby.com>
2018-07-23 19:06:03 +0530

    Upgrades the command utility

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const { logger, schemaBuilder, utils } = require('../lib')
+const fs = require('fs')
 
-const validateSchema =  (targetObject, options) => {
-  const path = options.filePath
-  invalidTarget = targetObject && typeof targetObject !== 'object'
-  if(invalidTarget)
-    return logger.error('Target must be an object')
-  if(!(targetObject || path))
+const validateSchema =  (targetObject, options = {}) => {
+  if(!targetObject)
     return logger.error('Missing argument: either targetObject or filePath is required')
+  let isPath = typeof targetObject === 'string'
+  if(isPath ? !fs.existsSync(targetObject) : typeof targetObject !== 'object')
+    return logger.error('Target must be either be an object or a valid filepath')
 
-  logger.info(`Validating schema for: ${options.filePath || 'targetObject'}`)
+  logger.info(`Validating schema for: ${targetObject}`)
   try {
     let inputSchema = options.schema || options.schemaPath || `${__dirname}/../examples/schema.json`
     const schema = schemaBuilder.getSchema(inputSchema)
-    const content = targetObject || utils.loadContent(options.filePath)
-    return _printErrors(schema.validate(content))
+    const content = isPath ? utils.loadContent(targetObject) : targetObject 
+    return printErrors(schema.validate(content))
   } catch (error) {
     logger.error(error)
   }
 }
 
-const _printErrors = errors => {
+const printErrors = errors => {
   if(errors.length) {
     logger.error(`====== Schema Validation Error ======\n\n${errors.length} mismatch found`)
     errors.forEach((err, index) => logger.red(`${index + 1}. ${err.message}`))
