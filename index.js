e535952b294f931fd506fc4d5b837f3ca5c2ecf0
ketanTechracers <ksaxena@gammastack.com>
2019-08-25 20:19:18 +0530

    Return errors array from validate method

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const pkg = require('./package.json')
 const validateSchema = require('./src')
 
 program
@@ -7,7 +11,7 @@ program
   .option('-t, --target [targetObj]', 'stringified JSON object whose structure is to be verified')
   .option('-j, --json','passed if target file is in JSON format')
   .option('-s, --schema [schemaPath]','path to an external schema file')
-  .action((filePath, options) => {
+  .action((options) => {
     const targetObj = options.targetObj && JSON.parse(options.targetObj)
     validateSchema(targetObj, options)
   });
