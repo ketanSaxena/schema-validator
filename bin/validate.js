a09019009674f0ee4c1d648e6a199425596df1a3
Ketan Saxena <ksaxena@grepruby.com>
2019-08-25 20:19:18 +0530

    Expose method validateSchema and update cli interface

--- bin/validate.js
+++ bin/validate.js
@@ -1,11 +1,14 @@
-const validateSchema = require('./src')
+#!/usr/bin/env node
+
+const program = require('commander')
+const pkg = require('../package.json')
+const validateSchema = require('../src')
 
 program
   .version(pkg.version)
   .command('validate')
   .option('-f, --filePath <filePath>', 'path to the target file for validating')
   .option('-t, --target [targetObj]', 'stringified JSON object whose structure is to be verified')
-  .option('-j, --json','passed if target file is in JSON format')
   .option('-s, --schema [schemaPath]','path to an external schema file')
   .action((filePath, options) => {
     const targetObj = options.targetObj && JSON.parse(options.targetObj)
@@ -13,10 +16,3 @@ program
   });
 
 program.parse(process.argv);
-
-if(process.argv.includes('help')) {
-  help()
-}
-
-module.exports = validateSchema
-module.exports.default = module.exports // For TypeScript
